f3806dea1a32d1001ee301945bf56f6ef701d331
valientjuno <jessedoake91@gmail.com>
2025-11-10 11:49:24 -0700

    week 2 correction final commit

--- week 2/task.py
+++ week 2/task.py
@@ -119,7 +119,6 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-Easy Level
 password = ""
 for char in range(0, nr_letters):
     password += random.choice(letters)
