226f195da7a0e565efd71f4c60d7e75ce414ce6c
valientjuno <jessedoake91@gmail.com>
2025-11-20 13:23:44 -0700

    turtle project final commit
    this commit details a turtle creation with python. In development the turtle is created, rendered a new color(DarkSeaGreen) and then moved forward a 100 paces.

---
+++ week 3/main3.py
@@ -0,0 +1,16 @@
+### Created Turtle, Changed its Color and Moved it 100 paces. ####
+
+import another_module
+print(another_module.another_variable)
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.fillcolor("DarkSeaGreen")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
